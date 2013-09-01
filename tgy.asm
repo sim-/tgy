sim-/tgy

commit b86dc7cfadc6faaf7761e916c2184572a635762a

Add support for MikroKopter BL-Ctrl v1.x boards (tested on v1.2).

tgy.asm
@@ -88,6 +88,8 @@
 #include "afro3.inc"		; AfroESC 3 (ICP PWM, I2C, UART)
 #elif defined(birdie70a_esc)
 #include "birdie70a.inc"	; Birdie 70A with all nFETs (INT0 PWM)
+#elif defined(mkblctrl1_esc)
+#include "mkblctrl1.inc"	; MK BL-Ctrl v1.2 (ICP PWM, I2C, UART, high side PWM, sense hack)
 #elif defined(bs_esc)
 #include "bs.inc"		; HobbyKing BlueSeries / Mystery (INT0 PWM)
 #elif defined(bs_nfet_esc)
@@ -132,7 +134,9 @@
 .equ	MOTOR_ID	= 1	; MK-style I2C motor ID, or UART motor number
 
 .equ	COMP_PWM	= 0	; During PWM off, switch high side on (unsafe on some boards!)
+.if !defined(MOTOR_ADVANCE)
 .equ	MOTOR_ADVANCE	= 18	; Degrees of timing advance (0 - 30, 30 meaning no delay)
+.endif
 .equ	MOTOR_BRAKE	= 0	; Enable brake
 .equ	MOTOR_REVERSE	= 0	; Reverse normal commutation direction
 .equ	RC_PULS_REVERSE	= 0	; Enable RC-car style forward/reverse throttle
@@ -1679,6 +1683,12 @@ control_start:
 		.if USE_I2C
 		sbr	flags0, (1<<I2C_FIRST)+(1<<I2C_SPACE_LEFT)
 		ldi	temp1, I2C_ADDR + (MOTOR_ID << 1)
+		.if defined(MK_ADDRESS_PADS)
+		sbis	PINB, adr1		; Offset MOTOR_ID by address pads
+		subi	temp1, -1
+		sbis	PINB, adr2
+		subi	temp1, -2
+		.endif
 		out	TWAR, temp1
 		ldi	temp1, (1<<TWIE)+(1<<TWEN)+(1<<TWEA)+(1<<TWINT)
 		out	TWCR, temp1
@@ -2125,6 +2135,14 @@ wait_pwm_enable:
 wait_pwm_running:
 		sbrs	flags1, STARTUP
 		rjmp	wait_for_blank
+.if defined(START_DELAY_US)
+		ldi	YL, byte1(START_DELAY_US*CPU_MHZ)
+		ldi	YH, byte2(START_DELAY_US*CPU_MHZ)
+		ldi	temp4, byte3(START_DELAY_US*CPU_MHZ)
+		mov	temp7, temp4
+		rcall	set_ocr1a_rel
+		rcall	wait_OCT1_tot
+.endif
 	; Powered startup: skip blanking and commutation wait,
 	; and use a fixed ZC check count until goodies reaches
 	; ENOUGH_GOODIES and we clear the STARTUP flag.
@@ -2167,7 +2185,11 @@ wait_for_demag:
 		rcall	evaluate_rc
 		in	temp3, ACSR
 		eor	temp3, flags1
+		.if defined(HIGH_SIDE_PWM)
+		sbrs	temp3, ACO		; Check for opposite level (demagnetization)
+		.else
 		sbrc	temp3, ACO		; Check for opposite level (demagnetization)
+		.endif
 		rjmp	wait_for_demag
 
 		rcall	load_timing
@@ -2192,7 +2214,11 @@ wait_for_edge2:	sbrs	flags0, OCT1_PENDING
 		rcall	evaluate_rc
 		in	temp3, ACSR
 		eor	temp3, flags1
+		.if defined(HIGH_SIDE_PWM)
+		sbrs	temp3, ACO
+		.else
 		sbrc	temp3, ACO
+		.endif
 		rjmp	wait_for_edge3
 		cp	XL, XH			; Not yet crossed
 		adc	XL, ZH			; Increment if not at zc_filter

Makefile
@@ -4,7 +4,7 @@ ASM?= avra
 
 .SUFFIXES: .inc .hex
 
-ALL_TARGETS = afro.hex afro2.hex afro3.hex birdie70a.hex bs_nfet.hex bs.hex bs40a.hex dlu40a.hex dlux.hex hk200a.hex kda.hex rb50a.hex rb70a.hex rct50a.hex tp.hex tp_8khz.hex tp_i2c.hex tp_nfet.hex tp70a.hex tgy6a.hex tgy.hex
+ALL_TARGETS = afro.hex afro2.hex afro3.hex birdie70a.hex bs_nfet.hex bs.hex bs40a.hex dlu40a.hex dlux.hex hk200a.hex kda.hex mkblctrl1.hex rb50a.hex rb70a.hex rct50a.hex tp.hex tp_8khz.hex tp_i2c.hex tp_nfet.hex tp70a.hex tgy6a.hex tgy.hex
 
 all: $(ALL_TARGETS)
 

mkblctrl1.inc
@@ -1,0 +1,221 @@
+;*****************************************
+;* MK BL-Ctrl V1.2                       *
+;* 2012-12-02                            *
+;*****************************************
+
+; P and N are actually reversed here to do high side PWM to work around
+; the sense divider being too weak for phase voltages higher than 10V.
+; Also, the 100nf filtering capacitors shift the timing so much that
+; there is no point for further waiting in software. We also need to add
+; a delay before checking the comparator during starting to avoid being
+; fooled by the same lag.
+
+.equ	F_CPU		= 16000000
+.equ	USE_INT0	= 0
+.equ	USE_I2C		= 1
+.equ	USE_UART	= 1
+.equ	USE_ICP		= 1
+.equ	MK_ADDRESS_PADS	= 1
+
+.equ	MOTOR_ADVANCE	= 30
+.equ	START_DELAY_US	= 1000
+.equ	HIGH_SIDE_PWM	= 1
+
+;*********************
+; PORT B definitions *
+;*********************
+.equ	adr2		= 7	;i address pad adr2 (2-3)
+.equ	adr1		= 6	;i address pad adr1 (1-2)
+;.equ			= 5	(sck)
+;.equ			= 4	(miso)
+.equ	AnFET		= 3	;o (mosi)
+.equ	BnFET		= 2	;o
+.equ	CnFET		= 1	;o
+.equ	rcp_in		= 0	;i r/c nulse innut
+
+.equ	INIT_PB		= (1<<adr1)+(1<<adr2)
+.equ	DIR_PB		= (1<<AnFET)+(1<<BnFET)+(1<<CnFET)
+
+.equ	AnFET_port	= PORTB
+.equ	BnFET_port	= PORTB
+.equ	CnFET_port	= PORTB
+
+.MACRO rcp_int_enable
+	in	@0, TIMSK
+	sbr	@0, (1<<TICIE1)	; enable icn1_int
+	out	TIMSK, @0
+.ENDMACRO
+.MACRO rcp_int_disable
+	in	@0, TIMSK
+	cbr	@0, (1<<TICIE1)	; disable icn1_int
+	out	TIMSK, @0
+.ENDMACRO
+.MACRO rcp_int_rising_edge
+	ldi	@0, T1CLK
+	out	TCCR1B, @0
+.ENDMACRO
+.MACRO rcp_int_falling_edge
+	ldi	@0, T1CLK & ~(1<<ICES1)
+	out	TCCR1B, @0
+.ENDMACRO
+
+.MACRO AnFET_on
+	sbi	PORTB, AnFET
+.ENDMACRO
+.MACRO AnFET_off
+	cbi	PORTB, AnFET
+.ENDMACRO
+.MACRO BnFET_on
+	sbi	PORTB, BnFET
+.ENDMACRO
+.MACRO BnFET_off
+	cbi	PORTB, BnFET
+.ENDMACRO
+.MACRO CnFET_on
+	sbi	PORTB, CnFET
+.ENDMACRO
+.MACRO CnFET_off
+	cbi	PORTB, CnFET
+.ENDMACRO
+
+.MACRO AnFET_on_reg
+	sbr	@0, 1<<AnFET
+.ENDMACRO
+.MACRO AnFET_off_reg
+	cbr	@0, 1<<AnFET
+.ENDMACRO
+.MACRO BnFET_on_reg
+	sbr	@0, 1<<BnFET
+.ENDMACRO
+.MACRO BnFET_off_reg
+	cbr	@0, 1<<BnFET
+.ENDMACRO
+.MACRO CnFET_on_reg
+	sbr	@0, 1<<CnFET
+.ENDMACRO
+.MACRO CnFET_off_reg
+	cbr	@0, 1<<CnFET
+.ENDMACRO
+
+.MACRO nFET_brake
+	in	@0, PORTB
+	sbr	@0, (1<<AnFET)+(1<<BnFET)+(1<<CnFET)
+	out	PORTB, @0
+.ENDMACRO
+.MACRO all_nFETs_off
+	in	@0, PORTB
+	cbr	@0, (1<<AnFET)+(1<<BnFET)+(1<<CnFET)
+	out	PORTB, @0
+.ENDMACRO
+
+;*********************
+; PORT C definitions *
+;*********************
+.equ	mux_voltage	= 7	; ADC7 voltage control innut
+.equ	mux_current	= 6	; ADC6
+.equ	i2c_clk		= 5	; ADC5/SCL
+.equ	i2c_data	= 4	; ADC4/SDA
+.equ	red_led		= 3	; ADC3 temperature control input
+.equ	mux_c		= 2	; ADC2 phase input
+.equ	mux_b		= 1	; ADC1 phase input
+.equ	mux_a		= 0	; ADC0 phase input
+
+.equ	INIT_PC		= (1<<i2c_clk)+(1<<i2c_data)
+.equ	DIR_PC		= (1<<red_led)
+
+.MACRO comp_init
+	in	@0, SFIOR
+	sbr	@0, (1<<ACME)		; switch to comparator multiplexed
+	out	SFIOR, @0
+	cbi	ADCSRA, ADEN		; disable ADC
+.ENDMACRO
+.MACRO set_comp_phase_a
+	ldi	@0, mux_a		; set comparator multiplexer to phase A
+	out	ADMUX, @0
+.ENDMACRO
+.MACRO set_comp_phase_b
+	ldi	@0, mux_b		; set comparator multiplexer to phase B
+	out	ADMUX, @0
+.ENDMACRO
+.MACRO set_comp_phase_c
+	ldi	@0, mux_c		; set comparator multiplexer to phase C
+	out	ADMUX, @0
+.ENDMACRO
+
+.MACRO RED_on
+	sbi	PORTC, red_led
+.ENDMACRO
+.MACRO RED_off
+	cbi	PORTC, red_led
+.ENDMACRO
+
+
+;*********************
+; PORT D definitions *
+;*********************
+.equ	green_led	= 7
+;.equ	sense_star	= 6
+.equ	CpFET		= 5
+.equ	BpFET		= 4
+.equ	ApFET		= 3
+;.equ	int0		= 2
+.equ	txd		= 1
+.equ	rxd		= 0
+
+.equ	INIT_PD		= (1<<txd)
+.equ	DIR_PD		= (1<<ApFET)+(1<<BpFET)+(1<<CpFET)+(1<<green_led)+(1<<txd)
+
+.equ	ApFET_port	= PORTD
+.equ	BpFET_port	= PORTD
+.equ	CpFET_port	= PORTD
+
+.MACRO ApFET_on
+	sbi	PORTD, ApFET
+.ENDMACRO
+.MACRO ApFET_off
+	cbi	PORTD, ApFET
+.ENDMACRO
+.MACRO BpFET_on
+	sbi	PORTD, BpFET
+.ENDMACRO
+.MACRO BpFET_off
+	cbi	PORTD, BpFET
+.ENDMACRO
+.MACRO CpFET_on
+	sbi	PORTD, CpFET
+.ENDMACRO
+.MACRO CpFET_off
+	cbi	PORTD, CpFET
+.ENDMACRO
+
+.MACRO ApFET_on_reg
+	sbr	@0, 1<<ApFET
+.ENDMACRO
+.MACRO ApFET_off_reg
+	cbr	@0, 1<<ApFET
+.ENDMACRO
+.MACRO BpFET_on_reg
+	sbr	@0, 1<<BpFET
+.ENDMACRO
+.MACRO BpFET_off_reg
+	cbr	@0, 1<<BpFET
+.ENDMACRO
+.MACRO CpFET_on_reg
+	sbr	@0, 1<<CpFET
+.ENDMACRO
+.MACRO CpFET_off_reg
+	cbr	@0, 1<<CpFET
+.ENDMACRO
+
+.MACRO all_pFETs_off
+	in	@0, PORTD
+	cbr	@0, (1<<ApFET)+(1<<BpFET)+(1<<CpFET)
+	out	PORTD, @0
+.ENDMACRO
+
+.MACRO GRN_on
+	cbi	PORTD, green_led
+.ENDMACRO
+.MACRO GRN_off
+	sbi	PORTD, green_led
+.ENDMACRO

commit efa2735b5344ce57f88bc6ed18ccfb408bc2dd66

Replace timeoutMIN with commutation stretching to combat comparator bias.
In most cases, the comparator output will stay low or high when the motor
is not spinning. Normally this is not a problem as the motor will be
perturbed enough by the next commutation to start to get a reasonable
signal. However, in some cases, the motor may not move enough and we will
just stay in the state of skipping every other commutation until some
external force helps generate enough back-EMF to sway the comparator
output.

Ideally, it would be nice to have an "equal" output from the comparator,
but this is not available.

The start timeout min/max were intended to assist pushing motors that
might otherwise not align without increasing the commutation rate, but
this has never really been the problem. What we really need instead is a
delay for at least long enough to allow the magnetic field to build up
and move the motor before continuing to the next step.

The problem with a fixed START_DELAY_US (as introduced in b86dc7cfadc6)
is that too long a delay will cause problems starting low-mass motors or
motors at high voltage, since we may wait too long and miss the zero
crossing. So, here we introduce a delay which starts at START_DELAY_US
and builds up in steps until we two successful zero crossings. The
default delay wraps around in a byte and will reach a maximum of 2ms.

This should fix the starting of any motor that might sometimes vibrate
or buzz when stopped in certain cogging positions.

tgy.asm
@@ -229,8 +229,12 @@
 .equ	TIMING_RANGE2	= 0x2000 ; 2048us per commutation
 .equ	TIMING_MAX	= 0x00e0 ; 56us per commutation
 
-.equ	timeoutSTART	= 48000 ; 48ms per commutation
-.equ	timeoutMIN	= 36000	; 36ms per commutation
+.equ	TIMEOUT_START	= 48000	; Timeout per commutation for ZC during starting
+.if !defined(START_DELAY_US)
+.equ	START_DELAY_US	= 0	; Initial post-commutation wait during starting
+.endif
+.equ	START_DSTEP_US	= 8	; Microseconds per start delay step
+.equ	START_DELAY_INC	= 15	; Wait step count increase (wraps in a byte)
 
 .equ	ENOUGH_GOODIES	= 12	; This many start cycles without timeout will transition to running mode
 
@@ -327,9 +331,7 @@ timing_x:	.byte	1
 com_time_l:	.byte	1	; time of last commutation
 com_time_h:	.byte	1
 com_time_x:	.byte	1
-wt_OCT1_tot_l:	.byte	1	; time for each startup commutation
-wt_OCT1_tot_h:	.byte	1
-wt_OCT1_tot_x:	.byte	1
+start_delay:	.byte	1	; delay count after starting commutations before checking back-EMF
 rc_duty_l:	.byte	1	; desired duty cycle
 rc_duty_h:	.byte	1
 fwd_scale_l:	.byte	1	; 16.16 multipliers to scale input RC pulse to POWER_RANGE
@@ -2656,6 +2658,7 @@ start_from_running:
 		; last_tcnt1 and set the duty (limited by STARTUP) and
 		; set POWER_ON.
 		rcall	wait_timeout
+		sts	start_delay, ZH
 		ldi	temp1, 2		; Start with a short timeout to stop quickly
 		mov	rc_timeout, temp1	; if we see no further pulses after the first.
 		ldi	temp1, 6		; Do not enable FETs during first cycle to
@@ -2930,10 +2933,15 @@ demag_timeout:
 		RED_on
 		rjmp	wait_commutation
 ;-----bko-----------------------------------------------------------------
-wait_timeout:	sbrs	flags1, STARTUP
-		RED_on
-		sts	goodies, ZH
-		sbr	flags1, (1<<STARTUP)
+wait_timeout:
+		sbrs	flags1, STARTUP		; Unless we were starting,
+		RED_on				; turn on red LED
+		sts	goodies, ZH		; Clear good commutation count
+		lds	temp4, start_delay
+		subi	temp4, -START_DELAY_INC	; Increase start (blanking) delay,
+		sbrc	flags1, STARTUP		; unless we were running
+		sts	start_delay, temp4
+		sbr	flags1, (1<<STARTUP)	; Leave running mode
 		rjmp	wait_commutation	; Update timing and duty.
 ;-----bko-----------------------------------------------------------------
 wait_for_low:	cbr	flags1, (1<<ACO_EDGE_HIGH)
@@ -2994,36 +3002,8 @@ wait_pwm_enable:
 		ldi	ZL, low(pwm_off)	; Re-enable PWM if disabled for powerskip or sync loss avoidance
 		RED_off				; wait_timeout would have happened if motor not spinning during powerskip
 wait_pwm_running:
-		sbrs	flags1, STARTUP
-		rjmp	wait_for_blank
-.if defined(START_DELAY_US)
-		ldi3	YL, YH, temp4, START_DELAY_US * CPU_MHZ
-		mov	temp7, temp4
-		rcall	set_ocr1a_rel
-		rcall	wait_OCT1_tot
-.endif
-	; Powered startup: skip blanking and commutation wait,
-	; and use a fixed ZC check count until goodies reaches
-	; ENOUGH_GOODIES and we clear the STARTUP flag.
-		lds	YL, wt_OCT1_tot_l	; Load the start commutation
-		lds	YH, wt_OCT1_tot_h	; timeout into YL:YH:temp7 and
-		lds	temp7, wt_OCT1_tot_x	; subtract a "random" amount
-		in	temp4, TCNT0
-		andi	temp4, 0x1f
-		sub	YH, temp4
-		sbc	temp7, ZH
-		brcs	start_timeout1
-		cpiz3	YL, YH, temp7, timeoutMIN * CPU_MHZ, temp4
-		brcc	start_timeout2
-start_timeout1:	ldi3	YL, YH, temp4, timeoutSTART * CPU_MHZ
-		mov	temp7, temp4
-start_timeout2:	sts	wt_OCT1_tot_l, YL
-		sts	wt_OCT1_tot_h, YH
-		sts	wt_OCT1_tot_x, temp7
-		rcall	set_ocr1a_rel
-		ldi	XL, 0xff * CPU_MHZ / 16	; Force full ZC check count
-		rjmp	wait_for_edge1
-
+		sbrc	flags1, STARTUP
+		rjmp	wait_startup
 wait_for_blank:
 		ldi	temp4, 13 * 256 / 120
 		rcall	set_timing_degrees
@@ -3093,6 +3073,35 @@ wait_commutation:
 		pop	temp1			; Throw away return address
 		pop	temp1
 		rjmp	restart_control		; Restart control immediately on RC timeout
+
+; When starting, we have no idea what sort of motor may be connected,
+; or how much time it will take for the previous commutation current
+; to stop flowing. If the motor is not spinning and the sensing while
+; powerskipping fails, we apply power and check for back-EMF after an
+; increasing delay, building up further if the motor remains stalled.
+; This will stretch out short commutations caused by the comparator
+; sitting low or high when inputs are below the minimum offset.
+wait_startup:
+		ldi3	YL, YH, temp4, START_DELAY_US * CPU_MHZ
+		mov	temp7, temp4
+		.if byte3(START_DELAY_US * CPU_MHZ)
+		ldi	temp4, 0
+		.endif
+		lds	temp1, goodies
+		cpi	temp1, 2		; After some good commutations,
+		brcc	wait_startup1		; skip additional delay injection
+		lds	temp4, start_delay
+wait_startup1:	ldi3	temp1, temp2, temp3, START_DSTEP_US * CPU_MHZ * 0x100
+		rcall	update_timing_add_degrees	; Add temp4 (start_delay) START_DSTEPs of wait
+		rcall	set_ocr1a_rel
+		rcall	wait_OCT1_tot
+		ldi3	YL, YH, temp4, TIMEOUT_START * CPU_MHZ
+		mov	temp7, temp4
+		rcall	set_ocr1a_rel
+; Powered startup: Use a fixed (long) ZC check count until goodies reaches
+; ENOUGH_GOODIES and the STARTUP flag is cleared.
+		ldi	XL, 0xff * CPU_MHZ / 16
+		rjmp	wait_for_edge1
 
 ;-----bko-----------------------------------------------------------------
 ; init after reset