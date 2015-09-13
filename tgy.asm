4f1f2b0b8c531153a6a897ffc767abb1a9d5d745
Simon Kirby <sim@netnation.com>
2015-09-12 22:27:17 -0700

    Replace empty LED macro definitions with automatic helper macros.

--- tgy.asm
+++ tgy.asm
@@ -609,6 +609,29 @@ eeprom_defaults_w:
 	.endif
 .endmacro
 
+;-- LED macros -----------------------------------------------------------
+
+.if !defined(red_led)
+	.macro RED_on
+	.endmacro
+	.macro RED_off
+	.endmacro
+.endif
+
+.if !defined(green_led)
+	.macro GRN_on
+	.endmacro
+	.macro GRN_off
+	.endmacro
+.endif
+
+.if !defined(blue_led)
+	.macro BLUE_on
+	.endmacro
+	.macro BLUE_off
+	.endmacro
+.endif
+
 ;-- FET driving macros ---------------------------------------------------
 ; Careful: "if" conditions split over multiple lines (with backslashes)
 ; work with arva, but avrasm2.exe silently produces wrong results.
